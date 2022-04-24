49a5db155a12ad075424f713c2854f2c39aab2c2
racheliyedigarov <101363844+racheliyedigarov@users.noreply.github.com>
2022-04-24 20:23:34 +0300

    final

--- Adv_C_Exe3/Main.c
+++ Adv_C_Exe3/Main.c
@@ -4,5 +4,31 @@
 
 void main()
 {
-	// add your code here
+
+	Stack rote;
+	initStack(&rote);
+	flipBetweenHashes("Remem#reb# thi#crap s#tice");
+	flipBetweenHashes("#meR#embe#siht r# practice");
+	push(&rote, 'R');
+	push(&rote, 'E');
+	push(&rote, 'M');
+	push(&rote, 'E');
+	push(&rote, 'M');
+	push(&rote, 'B');
+	push(&rote, 'E');
+	push(&rote, 'R');
+
+	Queue rote;
+	initQueue(&rote);
+	enqueue(&rote, 6);
+	enqueue(&rote, 1);
+	enqueue(&rote, 5);
+	enqueue(&rote, 2);
+	enqueue(&rote, 3);
+	enqueue(&rote, 1);
+	enqueue(&rote, 9);
+
+	rotateQueue(&rote);
+	cutAndReplace(&rote);
+	sortKidsFirst(&rote);
 }
--- Adv_C_Exe3/Queue.c
+++ Adv_C_Exe3/Queue.c
@@ -4,42 +4,125 @@
 
 void initQueue(Queue* q)
 {
-	// add your code here
+	q->top = NULL;
+	q->tail = NULL;
+	return;
 }
 
 void destroyQueue(Queue* q)
 {
-	// add your code here
+	int newnode;
+	list_delete2(s->head);
+	node* newnode = (node*)realloc(sizeof(node));
+	q->Queue;
+	return;
 }
 
 void enqueue(Queue* q, unsigned int data)
 {
-	// add your code here
+	intNode* new = (intNode*)malloc(sizeof(intNode));
+	new->next = NULL;
+	if (!isEmptyQueue(q)) q->tail->next = new;
+	else q->top = new;
+	q->tail = new;
 }
 
 unsigned int dequeue(Queue* q)
 {
-	// add your code here
+	if (*head == NULL)
+	{
+		printf("Stack is empty \n");
+		return 0;
+	}
+	else {
+		unsigned int tmp = q->top->data;
+		intNode* old = q->top;
+		q->head = q->top->next;
+		if (q->top == NULL) q->tail = NULL;
+		free(old);
+		return tmp;
+	}
 }
 
 int isEmptyQueue(const Queue* q)
 {
-	// add your code here
+	if (*head == NULL)
+		return 1;
+	else return 0;
+	q->tail == NULL && q->top == NULL;
 }
 
 /***************** Functions using Queues - Implementation/definition **************************/
 
 void rotateQueue(Queue* q)
 {
-	// add your code here
+
+	if (q == NULL) {
+		printf("the Queue is not invaild \n");
+	}
+	else {
+		Queue* tmp = (Queue*)malloc(sizeof(Queue));
+		initQueue(tmp);
+		while (q->head != q->tail) {
+			enqueue(tmp, dequeue(q));
+		}
+
+		while (!isEmptyQueue(tmp)) {
+			enqueue(q, dequeue(tmp));
+		}
+		q->Queue;
+	}
 }
 
 void cutAndReplace(Queue* q)
 {
-	// add your code here
+	unsigned int count = 0, num = 0, sum = 0;
+	Queue* t1 = (Queue*)malloc(sizeof(Queue));
+	Queue* t2 = (Queue*)malloc(sizeof(Queue));
+	initQueue(t1);
+	initQueue(t2);
+	while (!isEmptyQueue(q)) {
+		num = dequeue(q);
+		sum += num;
+		enqueue(t1, num);
+		count++;
+	}
+	if (count % 2 != 0) {
+		int avrg = sum / count;
+		enqueue(t1, avrg);
+		count++;
+	}
+	for (int i = 0; i < count / 2; i++) {
+		enqueue(t2, dequeue(t1));
+	}
+	while (!isEmptyQueue(tm)) {
+		rotateQueue(t1);
+		enqueue(q, dequeue(t1));
+	}
+	while (!isEmptyQueue(t2)) {
+		enqueue(q, dequeue(t2));
+	}
 }
 
 void sortKidsFirst(Queue* q)
 {
-	// add your code here
+	int val = 0;
+	Queue* tmp = (Queue*)malloc(sizeof(Queue));
+	initQueue(tmp);
+	while (!isEmptyQueue(q)) {
+		enqueue(tmp, dequeue(q));
+	}
+
+	while (!isEmptyQueue(tmp)) {
+		rotateQueue(tmp);
+		val = dequeue(tmp);
+		while (!isEmptyQueue(q) && q->tail > val) {
+			enqueue(tmp, dequeue(q));
+		}
+		enqueue(q, val);
+	}
+	while (!isEmptyQueue(tmp)) {
+		enqueue(q, dequeue(tmp));
+
+	}
 }
