a8d650a8d4d897b3bb5039ae8ce3576aed806ea6
racheliyedigarov <101363844+racheliyedigarov@users.noreply.github.com>
2022-04-24 18:46:46 +0300

    try3

--- Adv_C_Exe3/Stack.c
+++ Adv_C_Exe3/Stack.c
@@ -2,52 +2,137 @@
 
 /***************** Stack ADT Implementation *****************/
 
+typedef struct stack {
+	int* Items;
+	int stackSize;
+	int top;
+} stack;
+
 void initStack(Stack* s)
 {
 	S->top = NULL;
 	S->stack;
 	return;
-	S = !nullptr;
+
 }
 
 void destroyStack(Stack* s)
 {
 	int newnode;
+	list_delete(s->top);
 	node* newnode = (node*)realloc(sizeof(node));
 	S->stack;
 	return;
-	S = !nullptr;
+	s = !nullptr;
 }
 
 void push(Stack* s, char data)
 {
-	// add your code here
+	charNode* new = (charNode*)malloc(sizeof(charNode));
+	toAdd->next = top;
+	head = toAdd;
+	return top;
+	s->head = addToHead(s->top, new);
+	s = !nullptr;
 }
 
 char pop(Stack* s)
 {
-	// add your code here
+	int first;
+	if (*top == NULL)
+		printf("Stack is empty \n");
+	return 0;
+	first = s->top->data;
+	removeItem(&s->top);
+	return first;
 }
 
 
 int isEmptyStack(const Stack* s)
 {
-	// add your code here
+	s->stack;
+	if s > top = -1;
+	char isEmpty(const Stack_St * S)
+		return 1;
+	else
+		return 0;
+	s = !nullptr;
 }
 
 /*************** Functions using stacks - Implementation/definition **************************/
 
 void flipBetweenHashes(const char* sentence)
 {
-	// add your code here
+	unsigned int i = 0;
+	Stack tmp;
+	tmp.head = NULL;
+	while (sentence[i] != '\0') {
+		if (sentence[i] == '#') {
+			while (sentence[i + 1] != '#') {
+				push(&tmp, sentence[i + 1]);
+				i++;
+			}
+			while (!isEmptyStack(&tmp)) {
+				printf("%c", pop(&tmp));
+			}
+			i += 2;
+		}
+		else {
+			printf("%c", sentence[i]);
+			i++;
+		}
+	}
+	printf("\t");
+
+
 }
 
 int isPalindrome(Stack* s)
 {
-	// add your code here
+	char str[5];
+	int count = 0;
+	char tmp = 0;
+	if (*head == NULL)
+		printf("Stack is empty \n");
+	str[count] = pop(s);
+	count++;
+}
+	else count--;
+	for (int i = 0; i < count; i++) {
+		if (str[i] != str[count - i]) {
+			tmp = 0;
+		}
+		else tmp = 1;
+	}
+	return tmp;
 }
 
 void rotateStack(Stack* s, int n)
 {
-	// add your code here
+	{
+		while (n <= 0)
+			return;
+		unsigned int size = 0;
+		Stack t1, t2, t3;
+		initStack(&t1);
+		initStack(&t2);
+		initStack(&t3);
+		while (!isEmptyStack(s)) {
+			push(&t, pop(s));
+			size++;
+		}
+		if (n <= size) {
+			for (int i = 0; i < n; i++) {
+				push(&t2, pop(&t1));
+			}
+			while (!isEmptyStack(&t2)) {
+				push(&t3, pop(&t2));
+			}
+		}
+		while (!isEmptyStack(&t1)) {
+			push(s, pop(&t1));
+		}
+		while (!isEmptyStack(&t3)) {
+			push(s, pop(&t3));
+		}
 }
